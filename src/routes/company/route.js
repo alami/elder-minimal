92dea65d6bc09e0fdb7918b16de89e282488457d
alami <alami@km.ru>
2021-04-07 14:10:07 +0500

    src/route/company/route.js all:

--- src/routes/company/route.js
+++ src/routes/company/route.js
@@ -1,7 +1,14 @@
+const fetch = require('node-fetch');
 module.exports = {
-  all: () => [{ slug: '/company' }],
-  permalink: ({ request }) => request.slug,
-  data: () => {
+  all: async () => {
+    const companyData = await fetch(
+      'https://opendata.arcgis.com/datasets/a4d813c396934fc09d0b801a0c491852_0.geojson',
+    ).then((res) => res.json());
+    const companies = Object.values(companyData.features).map((entry) => entry.properties);
+    return companies.map((company) => ({ slug: company.FID }));
+  },
+  permalink: ({ request }) => `/${request.slug}/`,
+  data: async () => {
     return {};
   },
 };
